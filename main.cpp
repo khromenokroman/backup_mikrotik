43812189528e5a506aa9471b3be1ef3165ea6c11
khromenokroman <roma55592@yandex.ru>
2026-04-30 08:29:17 +0200

    feat: Добавление таймстампа в имя файла резервной копии

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ class Backup {
 
         auto back = m_client.execute("export show-sensitive");
         std::ranges::remove(back, '\r');
-        auto file_bk = base / m_device_name / "backup";
+        auto file_bk = base / m_device_name / ::fmt::format("backup_{}", get_current_time_for_filename());
         std::ofstream file(file_bk);
         if (!file.is_open()) {
             auto err = errno;
@@ -27,6 +27,19 @@ class Backup {
     }
 
    private:
+    std::string get_current_time_for_filename() {
+        const auto now = std::chrono::system_clock::now();
+        const std::time_t time = std::chrono::system_clock::to_time_t(now);
+
+        std::tm tm{};
+        localtime_r(&time, &tm);
+
+        std::ostringstream oss;
+        oss << std::put_time(&tm, "%Y-%m-%d_%H-%M-%S");
+
+        return oss.str();
+    }
+
     std::string get_name_device() {
         auto a = m_client.execute("system/identity/print");
         std::ranges::remove(a, '\r');
